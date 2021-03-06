0a28cbd5f7add7633a7e1daa8b995eea1b4161d9
İsmail Kılıç <152120171049@ogrenci.ogu.edu.tr>
2021-03-06 21:24:49 +0300

    upload project

---
+++ OnlineBookStore/OnlineBookStore.UI/frmMain.cs
@@ -0,0 +1,68 @@
+﻿using OnlineBookStore.Business.Abstract;
+using OnlineBookStore.Business.Concrete;
+using OnlineBookStore.DataAccess.Concrete.EntityFramework;
+using OnlineBookStore.Entities.Concrete;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OnlineBookStore.UI
+{
+    public partial class FrmMain : Form
+    {
+        private Form _activeForm = null;
+        public void openChildForm(Form childForm)
+        {
+            _activeForm?.Close();
+
+            _activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            btnUserInfos.Text = LoginedCustomer.GetLoginedCustomer.Email;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            openChildForm(new FrmBooks());          
+        }
+
+        private void btnBooks_Click(object sender, EventArgs e)
+        {
+            openChildForm(new FrmBooks());
+        }
+
+        private void btnUserInfos_Click(object sender, EventArgs e)
+        {
+            openChildForm(new FrmCustomerInfos());
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            var dialog = MessageBox.Show("Programdan çıkmak istediğinize emin misiniz?", "Çıkış", MessageBoxButtons.YesNo);
+
+            if (dialog != DialogResult.Yes) return;
+
+            Application.Exit();
+        }
+
+        private void btnChart_Click(object sender, EventArgs e)
+        {
+            openChildForm(new FrmShoppingCart());
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+            openChildForm(new FrmOrders());
+        }
+    }
+}
